c9513f0383f1b79279295dffe0762b3e7f09760c
Maciej Korsan <maciek@cotenfrontend.pl>
2023-08-27 19:31:58 +0200

    initial configuration, markup & styling

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        'roboto': ['roboto', 'sans-serif']
+      }
+    },
+  },
+  plugins: [],
+}
+
