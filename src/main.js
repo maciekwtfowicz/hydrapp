930944af6c34ba3803b9d422907b9d5111bc7fb4
Maciej Korsan <maciek@cotenfrontend.pl>
2023-08-27 22:07:17 +0200

    localstorage support

--- src/main.js
+++ src/main.js
@@ -4,8 +4,17 @@ const counterValue = document.querySelector('.counter--js');
 
 let glasses = 0;
 
+const key = new Date().toISOString().slice(0,10);
+
+
+if (localStorage.getItem(key)) {
+  glasses = parseInt(localStorage.getItem(key));
+}
+
+
 const setCounterValue = (value) => {
   counterValue.innerHTML = value;
+  localStorage.setItem(key, value);
 }
 
 setCounterValue(glasses)
