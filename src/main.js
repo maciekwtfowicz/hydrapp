69b442a6ac27fdf71fbe30d70796d11e8891bffe
Maciej Korsan <maciek@cotenfrontend.pl>
2023-09-13 22:17:45 +0200

    Add grouping for dates / reset the counter after midnight

--- src/main.js
+++ src/main.js
@@ -4,12 +4,24 @@ const counterValue = document.querySelector('.counter--js');
 
 let glasses = 0;
 
-const key = new Date().toISOString().slice(0,10);
+let key = new Date().toLocaleString().slice(0,10);
+
+setInterval(() => {
+  console.log('tik-tak')
+  if(key !== new Date().toLocaleString().slice(0,10)) {
+    key = new Date().toLocaleString().slice(0,10)
+    setCounterValue(0)
+    glasses=0
+  }
+}, 1000*60 )
+
 
 
 if (localStorage.getItem(key)) {
   glasses = parseInt(localStorage.getItem(key));
 }
+
+
 
 
 const setCounterValue = (value) => {
